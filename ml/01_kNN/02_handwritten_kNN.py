322c4b2a668ad33b55cf365a4aad0826ccc73d3c
liuyingdi <liuyingdi_ai@163.com>
2020-05-19 21:42:35 +0800

    handwritten_kNN introduce 'weights' parameter

--- ml/01_kNN/02_handwritten_kNN.py
+++ ml/01_kNN/02_handwritten_kNN.py
@@ -3,19 +3,23 @@ from collections import Counter
 from sklearn import datasets
 from ml.tools.model_selection import train_test_split
 from ml.tools.metrics import accuracy_score
+from ml.tools.preprocessing import standardization
 
 random_state = 666
 np.random.seed(random_state)
 
 
 class KNeighborsClassifier:
-    def __init__(self, n_neighbors=5, p=2):
+    def __init__(self, n_neighbors=5, p=2, weights=False):
         self.n_neighbors = n_neighbors
         self.p = p
+        self.weights = weights
         self.X = None
         self.y = None
 
     def fit(self, X_train, y_train):
+        if self.weights:
+            X_train = standardization(X_train)
         self.X = X_train
         self.y = y_train
 
@@ -31,9 +35,27 @@ class KNeighborsClassifier:
         sort_indexes = np.argsort(distance)
         y_sort = self.y[sort_indexes[:self.n_neighbors]]
 
-        counter = Counter(y_sort)
-        vote = counter.most_common()
-        y_predict = vote[0][0]
+        if self.weights:
+            # weights=1/distance
+            distance = distance[sort_indexes[:self.n_neighbors]]
+            vote_dictionary = {}
+            for index in range(self.n_neighbors):
+                d = distance[index]
+                cls = y_sort[index]
+                if vote_dictionary.keys().__contains__(cls):
+                    vote_dictionary[cls] += 1 / max(1e-8, d.item())
+                else:
+                    vote_dictionary[cls] = 1 / max(1e-8, d.item())
+            max_value = 0
+            y_predict = None
+            for key, value in vote_dictionary.items():
+                if value > max_value:
+                    max_value = value
+                    y_predict = key
+        else:
+            counter = Counter(y_sort)
+            vote = counter.most_common()
+            y_predict = vote[0][0]
         return y_predict
 
     def predict(self, X_predict):
@@ -41,6 +63,8 @@ class KNeighborsClassifier:
         return y_predict
 
     def score(self, X_test, y_test):
+        if self.weights:
+            X_test = standardization(X_test)
         y_predict = self.predict(X_predict=X_test)
         score = accuracy_score(y_true=y_test, y_pred=y_predict)
         return score
@@ -62,7 +86,7 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
-    knn_clf = KNeighborsClassifier(n_neighbors=5, p=2)
+    knn_clf = KNeighborsClassifier(n_neighbors=5, p=2, weights=True)
     knn_clf.fit(X_train=X_train, y_train=y_train)
 
     score = knn_clf.score(X_test=X_test, y_test=y_test)
--- ml/tools/metrics.py
+++ ml/tools/metrics.py
@@ -5,3 +5,7 @@ def accuracy_score(y_true, y_pred):
     assert y_true.shape == y_pred.shape
     score = np.sum(y_true == y_pred) / len(y_true)
     return score
+
+
+def r2_score():
+    pass
