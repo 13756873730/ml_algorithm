8809679ed92e491c92dddbe1cff90d0b34cda130
liuyingdi <liuyingdi_ai@163.com>
2020-05-22 17:11:56 +0800

    logistic regression: One vs Rest/One vs One

--- 07_LogisticRegression/03_sklearn_OvR_OvO.py
+++ 07_LogisticRegression/03_sklearn_OvR_OvO.py
@@ -13,9 +13,6 @@ def load_data():
     iris = datasets.load_iris()
     X = iris.data
     y = iris.target
-    # binary classifier
-    X = X[y <= 1]
-    y = y[y <= 1]
     # shuffle
     shuffle_indexes = np.random.permutation(len(X))
     X, y = X[shuffle_indexes], y[shuffle_indexes]
@@ -28,9 +25,19 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
     log_reg = LogisticRegression(penalty='l2', C=1.0)
-    log_reg.fit(X_train, y_train)
 
-    score = log_reg.score(X_test, y_test)
-    print(score)  # 1.0
+    # One vs Rest
+    # log_reg_ovr = LogisticRegression(penalty='l2', C=1.0, multi_class='ovr')
+    log_reg_ovr = OneVsRestClassifier(estimator=log_reg)
+    log_reg_ovr.fit(X_train, y_train)
+    score = log_reg_ovr.score(X_test, y_test)
+    print('OvR score: {}'.format(score))  # 0.8666666666666667
+
+    # One vs One
+    # log_reg_ovo = LogisticRegression(penalty='l2', C=1.0, multi_class='multinomial')
+    log_reg_ovo = OneVsOneClassifier(estimator=log_reg)
+    log_reg_ovo.fit(X_train, y_train)
+    score = log_reg_ovo.score(X_test, y_test)
+    print('OvO score: {}'.format(score))  # 0.9666666666666667
 
     pass
